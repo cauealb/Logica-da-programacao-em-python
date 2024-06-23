13d0e286dd3aeda9d5eee9acb5a74e08c1744bed
Cauê Alves <167707073+cauealb@users.noreply.github.com>
2024-06-23 10:29:14 -0300

    alteração no repo remoto4

--- ExercíciosRepetitivos2/Repetitivos(for)8.py
+++ ExercíciosRepetitivos2/Repetitivos(for)8.py
@@ -1,9 +1,7 @@
 # Faça um programa que leia 5 números e informe a soma e a média dos números.
 total = 0
-contador = 0
 
 for i in range(1, 5 + 1):
-    contador += 1
     num = int(input('Digite um número: '))
     total += num
 media = total / i
