30acd556a1f510e39e115906f92e3ebea8faad6d
Cauê Alves <cauealvesb4@gmail.com>
2024-07-06 19:12:21 -0300

    commit

--- Matrizes1/matriz7.py
+++ Matrizes1/matriz7.py
@@ -6,3 +6,85 @@
 # d) imprimir os elementos da diagonal principal da matriz.
 # e) alterar a matriz elevando ao quadrado todos os números negativos da mesma. Em seguida imprimir
 # a matriz alterada. 
+
+o =int(input('Qual a ordem da matriz: '))
+matriz = [[0 for _ in range(o)] for _ in range(o)]
+totalP = 0
+vetorqb = []
+vetorqc = []
+vetordiag = []
+
+for l in range(o):
+    for c in range(o):
+        num = float(input(f'Elemento [{l},{c}]: '))
+        matriz[l][c] = num
+        
+        #Soma dos positivos
+        if num >= 0:
+            totalP += num
+
+print(f'\nSoma dos Positivos: {totalP}\n')
+
+
+
+
+
+qb = int(input('Escolha uma linha: '))
+
+for l in range(o):
+    for c in range(o):
+        if l == qb:
+            valor = matriz[l][c]
+            vetorqb.append(valor)
+
+print('Linha escolhida: ', end='')
+for i in vetorqb:
+    print(i, end=' ')
+
+
+
+
+
+qc = int(input('\nEscolha uma coluna: '))
+for l in range(o):
+    for c in range(o):
+        if c == qc:
+            valor = matriz[l][c]
+            vetorqc.append(valor)
+
+print('Linha escolhida: ', end='')
+for i in vetorqc:
+    print(i, end=' ')
+
+
+
+
+
+
+print('\nLinha diagonal: ', end='')
+for l in range(o):
+    for c in range(o):
+        if l == c:
+            valor = matriz[l][c]
+            vetordiag.append(valor)
+
+print('\nDiagonal Principal\n')
+for i in vetordiag:
+    print(i, end=' ')
+
+
+print('\nMatriz Alterada:\n')
+
+for l in range(o):
+    for c in range(o):
+        if matriz[l][c] < 0:
+            valor = matriz[l][c]
+            del matriz[l][c]
+            conta = valor ** 2
+            matriz[l].insert(c, conta)
+
+for l in range(o):
+    for c in range(o):
+        print(c, end='')
+    print('\n')
+
