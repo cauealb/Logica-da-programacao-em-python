00c43edf1dafa1bb1b9c31ad172912c042b90d2e
Cauê Alves <cauealvesb4@gmail.com>
2024-08-02 16:57:23 -0300

    commit

--- Data Structures/linkedList.js
+++ Data Structures/linkedList.js
@@ -192,6 +192,30 @@ class doublyLinkedList{
         this.length = 0
     }
 
+    push(elem){
+        const node = new Node(elem)
+
+        if(!this.head){
+            this.head = node
+
+            this.length++
+            return
+        }
+
+        let currentNode = this.head
+        while(currentNode.next){
+            currentNode = currentNode.next
+        }
+
+        node.prev = currentNode
+        currentNode.next = node
+        
+        this.length++
+        return
+    }
+
+
+
     addFirst(elem){
         const node = new Node(elem);
         node.nex = this.head
@@ -266,6 +290,8 @@ class doublyLinkedList{
         }
         if(!this.head.next){
             this.head = null
+            this.length--
+            return
         }
 
         let currentNode = this.head
@@ -275,11 +301,48 @@ class doublyLinkedList{
 
         currentNode.next = null
         currentNode.next.prev = null
+        this.length--
     }
 
 
+    removeAt(index){
+        if(index > this.length || index < this.length){
+            return false
+        }
 
+        if(!this.head){
+            return false
+        }
 
+        let currentNode = this.head
+        if(index === 0){
+            currentNode.next.prev = null
+            if(currentNode,next){
+                this.head = currentNode.next
+            }
+            this.length--
+            return
+        }
+
+        
+        for(let i = 0; i < index; i++){
+            lastNode = currentNode
+            currentNode = currentNode.next
+        }
+        let lastNode = currentNode.prev
+        let nextNode = currentNode.next
+
+        if(nextNode){
+            lastNode.next = nextNode
+            nextNode.prev = lastNode   
+        }
+
+        currentNode.next = null
+        currentNode.prev = null
+
+        this.length-- 
+
+    }
 
 
     print(){
@@ -294,7 +357,8 @@ class doublyLinkedList{
 
 const list = new doublyLinkedList()
 
-list.addFirst('Cauê')
+list.push('Cauê')
+list.push('Alves')
 
 
 list.print()
