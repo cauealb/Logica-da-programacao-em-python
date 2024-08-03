f881e5d46e1d80d68fdc1de89de8a8f2e9fa1014
Cauê Alves <cauealvesb4@gmail.com>
2024-08-03 08:29:40 -0300

    commit

--- Arrays/js.js
+++ Arrays/js.js
@@ -141,7 +141,7 @@ class linked{
     constructor(){
         this.length = 0;
         this.head = null;
-    }
+    };
 
     print(){
         if(!this.head){
@@ -149,36 +149,120 @@ class linked{
         }
 
         let currentNode = this.head
-        while(currentNode.next){
+        while(currentNode){
             console.log(currentNode.elem)
             currentNode = currentNode.next
         }
-    }
+    };
 
     push(elem){
-        let node = new Node(elem)
+        const node = new Node(elem)
 
         if(!this.head){
             this.head = node
             this.length++
             return
+        }else{
+            let currentNode = this.head
+            while(currentNode.next){
+                currentNode = currentNode.next
+            }
+            currentNode.next = node
+        }
+        this.length++
+    };
+
+    pop(){
+        if(!this.head){
+            return false
+        }
+        if(!this.head.next){
+            this.head = null
+            this.length--
+            return 
         }
 
         let currentNode = this.head
-        while(currentNode){
+        let previus
+        while(currentNode.next){
+            previus = currentNode
             currentNode = currentNode.next
         }
 
-        node.prev = currentNode
-        this.length++
+        previus.next = null
+        this.length--
     }
-}
+
+    pushMidIndex(index, elem){
+        const node = new Node(elem)
+
+        if(index < 0 || index > this.length){
+            return false
+        }
+        if(!this.head.next){
+            this.head = null
+            this.length--
+            return
+        }
+
+        let currentNode = this.head
+        let previus
+        for(let i = 0; i < index; i++){
+            previus = currentNode
+            currentNode = currentNode.next
+        }
+
+        if(currentNode.next){
+            previus.next = node
+            node.next = currentNode
+        }
+        this.length--
+    }
+
+    popMidIndex(index){
+        if(index < 0 || index > this.length){
+            return false
+        }
+        if(index === 0){
+            this.head.next = this.head
+            this.length--
+        }
+
+            let currentNode = this.head
+            let previus
+            for(let i = 0; i < index; i++){
+                previus = currentNode
+                currentNode = currentNode.next
+            }
+
+            if(currentNode.next){
+                previus.next = currentNode.next
+            }
+            currentNode = null
+            this.length--
+        }
+
+
+        indexOf(index){
+            if(index < 0 || index >= this.length){
+                return -1
+            }
+
+            let currentNode = this.head
+            for(let i = 0; i < index; i++){
+                currentNode = currentNode.next
+            }
+            
+            return currentNode.elem
+        }
+    }
+
 
 const list = new linked()
 
-list.push(8)
-list.push(1)
-list.push(1)
-list.push(6)
+list.push('Cauê')
+list.push('Alves')
+list.push('Geovanna')
+list.push('Alves')
 
-list.print()
+
