5f154e6d08b66d788d3fc806710ecd689703cb94
Cauê Alves <cauealvesb4@gmail.com>
2024-08-13 16:17:24 -0300

    commit

--- Arrays/ipgkjw.js
+++ Arrays/ipgkjw.js
@@ -1,49 +1,47 @@
-const isValid = function(s){
+var isValid = function(s) {
     const splitA = s.split('')
     let array = []
-    let j 
-    for(let i = 0; i < splitA.length; i++){
-    
-        if(splitA[i] === '(' || splitA[i] === '{' || splitA[i] === '['){
-            array.push(splitA[i])
-        }
-        else{
-            if(array.length === 0){
-                return true;;
-            }
-    
-            j = array.length - 1
-            switch (array[j]){
-                case '(':
-                    if(splitA[i] === ')'){
-                        array.pop()
-                    }else{
-                        return false;
-                    }
-                    break;
-                case '{':
-                    if(splitA[i] === '}'){
-                        array.pop()
-                    }else{
-                        return false;
-                    }
-                    break;
-                case '[':
-                    if(splitA[i] === ']'){
-                        array.pop()
-                    }else{
-                        return false;
-                    }
-                    break;
-                default:
-                    return false;
-            }
-        }
-    }
-    if(j !== 0){
-        return false;
-    }
-    return true;
-}
+    let j
 
-isValid(']')
+for(let i = 0; i < splitA.length; i++){
+    
+    if(splitA[i] === '(' || splitA[i] === '{' || splitA[i] === '['){
+        array.push(splitA[i])
+    }
+    else{
+
+        j = array.length - 1
+        switch (array[j]){
+            case '(':
+                if(splitA[i] === ')'){
+                    array.pop()
+                }else{
+                    return false;
+                }
+                break;
+            case '{':
+                if(splitA[i] === '}'){
+                    array.pop()
+                }else{
+                    return false;
+                }
+                break;
+            case '[':
+                if(splitA[i] === ']'){
+                    array.pop()
+                }else{
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+    }
+}
+if(j !== 0 || array.length === 1){
+    return false;
+}
+return true;
+};
+
+isValid('([]){')
