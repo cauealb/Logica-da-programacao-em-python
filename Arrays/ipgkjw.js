45c77ab4fd8a071c680bc5af550548764a068631
Cauê Alves <cauealvesb4@gmail.com>
2024-08-14 16:20:16 -0300

    commit

--- Arrays/ipgkjw.js
+++ Arrays/ipgkjw.js
@@ -1,47 +1,46 @@
-var isValid = function(s) {
-    const splitA = s.split('')
-    let array = []
-    let j
+// var clearDigits = function(s) {
+//     const splitA = s.split('');
+//     let array = [];
+//     let j, number, cont = -1;
 
-for(let i = 0; i < splitA.length; i++){
-    
-    if(splitA[i] === '(' || splitA[i] === '{' || splitA[i] === '['){
-        array.push(splitA[i])
-    }
-    else{
+//     for(let i of splitA){
+//         number = Number(i);
+//         if(number >= 0){
+//             array.push(number);
+//         }else{
+//             array.push(i);
+//         }
+//     }
 
-        j = array.length - 1
-        switch (array[j]){
-            case '(':
-                if(splitA[i] === ')'){
-                    array.pop()
-                }else{
-                    return false;
-                }
-                break;
-            case '{':
-                if(splitA[i] === '}'){
-                    array.pop()
-                }else{
-                    return false;
-                }
-                break;
-            case '[':
-                if(splitA[i] === ']'){
-                    array.pop()
-                }else{
-                    return false;
-                }
-                break;
-            default:
-                return false;
+//     for(let i of array){
+//         cont++;
+//         j = cont - 1;
+//         if(typeof i === 'number'){ 
+//             array.splice(cont, 1);
+//             array.splice(j, 1);
+//         }
+//     }
+//     return array;
+// };
+
+
+
+
+const clearElem = function(s){
+    const splitA = s.split('');
+    let array = [];
+    let number;
+
+    for(let i of splitA){
+        number = Number(i)
+        if(i >= 0){
+            array.pop()
+        }else{
+            array.push(i)
         }
     }
+    let answer = array.join('').trim()
+    return answer;
 }
-if(j !== 0 || array.length === 1){
-    return false;
-}
-return true;
-};
 
-isValid('([]){')
+clearElem('abc')
