7535ea142576c1bf86e582a4dc5a167bb42dbfaa
Cauê Alves <cauealvesb4@gmail.com>
2024-08-15 18:56:13 -0300

    commit

--- Arrays/ipgkjw.js
+++ Arrays/ipgkjw.js
@@ -1,46 +1,33 @@
-// var clearDigits = function(s) {
-//     const splitA = s.split('');
-//     let array = [];
-//     let j, number, cont = -1;
+var removeOuterParentheses = function(s) {
+    const trus = s.split('')
+    let array = []
+    let antigo
 
-//     for(let i of splitA){
-//         number = Number(i);
-//         if(number >= 0){
-//             array.push(number);
-//         }else{
-//             array.push(i);
-//         }
-//     }
+    for(let i = 0; i < trus.length; i++){
+        antigo = i - 1
+        if(array.length >= 1){
+            if(trus[i]!== trus[antigo])
+                array.push(trus[i])
 
-//     for(let i of array){
-//         cont++;
-//         j = cont - 1;
-//         if(typeof i === 'number'){ 
-//             array.splice(cont, 1);
-//             array.splice(j, 1);
-//         }
-//     }
-//     return array;
-// };
+        }else if(){
 
-
-
-
-const clearElem = function(s){
-    const splitA = s.split('');
-    let array = [];
-    let number;
-
-    for(let i of splitA){
-        number = Number(i)
-        if(i >= 0){
-            array.pop()
-        }else{
-            array.push(i)
+        }
+        
+        
+        else{
+            if(trus[i] === trus[antigo]){
+                array.push(trus[i])
+            }
         }
     }
-    let answer = array.join('').trim()
-    return answer;
-}
 
-clearElem('abc')
+    let string = array.join('')
+    return string
+};
+
+
+
+
+
+
+removeOuterParentheses('(()(()))')
