2ba538d081ff6f72b5078df17b1b9c490f7a87e3
Cauê Alves <cauealvesb4@gmail.com>
2024-08-25 13:47:57 -0300

    commit

--- Arrays/ipgkjw.js
+++ Arrays/ipgkjw.js
@@ -1,41 +1,83 @@
-function heapFy(arr, length, parentIndex){
-    let largest = parentIndex;
-    let left = parentIndex * 2 + 1;
-    let rigth = left + 1;
-
-    if(left < length && arr[left] < arr[largest]){
-        largest = left;
+class PQHeap{
+    constructor(){
+        this.heap = []
     }
 
-    if(rigth < length && arr[rigth] < arr[largest]){
-        largest = rigth;
+    print(){
+        if(this.heap.length === 0){
+            return false
+        }
+
+        for(let index of this.heap){
+            console.log(index[0] + ', priority: ' + index[1])
+        }
     }
 
-    if(parentIndex !== largest){
-        [arr[largest], arr[parentIndex]] = [arr[parentIndex], arr[largest]];
-
-        heapFy(arr, length, largest);
+    swap(i, j){
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]]
     }
 
-    return arr;
+    heapFyUp(){
+        let index = this.heap.length - 1
+        
+        while(index > 0){
+        let parentIndex = Math.floor((index - 1) / 2)
+            if(this.heap[index][1] < this.heap[parentIndex][1]){
+                break;
+            }
+
+            this.swap(index, parentIndex)
+            index = parentIndex
+        }
+    }
+
+    heapFyDown(){
+        let index = 0
+        const length = this.heap.length
+
+        while(true){
+            let left = Math.floor(index * 2 + 1)
+            let rigth = left + 1
+            let smallest = index
+
+            if(left < length && this.heap[left] > this.heap[smallest]){
+                smallest = left
+            }
+            if(rigth < length && this.heap[rigth] > this.heap[smallest]){
+                smallest = rigth
+            }
+
+            if(smallest === index){
+                break;
+            }
+
+            this.swap(index, smallest)
+            index = smallest
+        }
+    }
+
+    enqueue(elem){
+        this.heap.push(elem)
+        this.heapFyUp()
+    }
+
+    dequeue(){
+        if(this.heap.length === 0) return false;
+        if(this.heap.length === 1) this.heap.pop();
+
+        const peek = this.heap.pop()
+        this.heap[0] = peek
+        this.heapFyDown()
+        return peek
+    }
 }
 
-function heapSort(arr){
-    let length = arr.length;
-    let lastParentNode = Math.floor(length / 2 - 1);
-    let lastChild = length - 1;
+const heap = new PQHeap()
 
-    while(lastParentNode >= 0){
-        heapFy(arr, length, lastParentNode);
-        lastParentNode--;
-    }
+heap.enqueue(['Cauê', 2])
+heap.enqueue(['Geovanna', 3])
+heap.enqueue(['Alves', 5])
+heap.enqueue(['Safira', 7])
+heap.enqueue(['Vasconscelos', 1])
 
-    while(lastChild >= 0){
-        heapFy(arr, lastChild, 0);
-        lastChild--;
-    }
-
-    return arr;
-}
-
-console.log(heapSort([4, 7, 9, 4, 3, 6, 2]))
+heap.print()
