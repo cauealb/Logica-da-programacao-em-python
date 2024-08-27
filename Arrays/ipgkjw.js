9de26cddef3ee9c9a0b6005d89d260337048939e
Cauê Alves <cauealvesb4@gmail.com>
2024-08-27 16:25:25 -0300

    commit

--- Arrays/ipgkjw.js
+++ Arrays/ipgkjw.js
@@ -1,99 +1,37 @@
-class PQ{
-    constructor(){
-        this.queue = []
-    }
-
-    print(){
-        if(this.queue.length === 0){
-            return false;
-        }
-
-        for(let index of this.queue){
-            console.log(index[0], ', priority: ', index[1])
-        }
-    }
-
-    swap(i, j){
-        [this.queue[i], this.queue[j]] = [this.queue[j], this.queue[i]]
-    }
-
-    heapfyUp(){
-        let index = this.queue.length - 1
-        let parent = Math.floor((index - 1) / 2)
-        
-        while(index > 0 && this.queue[index][1] > this.queue[parent][1]){
-            this.swap(index, parent)
-            index = parent
-            parent = Math.floor((index - 1) / 2)
-        }
-    }
-
-    heapfyDown(){
-        let index = 0
-        let length = this.queue.length
-
-        while(true){
-            let left = index * 2 + 1
-            let rigth = left + 1
-            let largest = index
-
-            if(left < length && this.queue[left][1] > this.queue[largest][1]){
-                largest = left
-            }
-
-            if(rigth < length && this.queue[rigth][1] > this.queue[largest][1]){
-                largest = rigth
-            }
-
-            if(largest === index){
-                break;
-            }
-
-            this.swap(index, largest)
-            index = largest
-        }
-    }
-
-    enqueue(elem){
-        this.queue.push(elem)
-        this.heapfyUp()
-    }
-
-    dequeue(){
-        if(this.queue.length === 0) return false;
-        if(this.queue.length === 1) this.queue.pop();
-
-        let peek = this.queue[0]
-        let last = this.queue.pop()
-        if(this.queue.length > 0){
-            this.queue[0] = last
-            this.heapfyDown()
-        }
-    }
-
-    increasePriority(elem){
-        const index = this.queue.findIndex(i => i[0] === elem[0])
-        
-        if(index === -1){
-            return false
-        }
-
-        if(elem[1] > this.queue[index][1]){
-            this.queue[index][1] = elem[1]
-            this.heapfyDown()
-        }
-    }
+function swap(arr, i, j){
+    [arr[i], arr[j]] = [arr[j], arr[i]]
 }
 
-const qp = new PQ()
+function heapFy(arr, lenght, parentIndex){
+    let largest = parentIndex
+    let left = parentIndex * 2 + 1
+    let rigth = left + 1
 
-qp.enqueue(['Cauê', 5])
-qp.enqueue(['Geovanna', 2])
-qp.enqueue(['Alves', 1])
-qp.enqueue(['Vasconscelos', 4])
-// Cauê, Vasconcelos, Geovanna, Alves
-qp.increasePriority(['Alves', 7])
-qp.increasePriority(['Cauê', 6])
-qp.increasePriority(['Geovanna', 1])
+    if(left < lenght && arr[left] > arr[largest]){
+        largest = left
+    }
+    if(rigth < lenght && arr[rigth] > arr[largest]){
+        largest = rigth
+    }
 
-qp.print()
+    if(parentIndex !== largest){
+        swap(arr, parentIndex, largest)
+        heapFy(arr, lenght, largest)
+    }
+
+    return arr;
+}
+
+function heapSort(arr){
+    let lenght = arr.length 
+    let index = lenght - 1
+    let indexParent = Math.floor((index - 1) / 2)
+
+    while(indexParent >= 0){
+        heapFy(arr, lenght, indexParent)
+        indexParent--
+    }
+    return arr;
+}
+
+console.log(heapSort([5, 7, 8, 3, 4, 2, 6]))
