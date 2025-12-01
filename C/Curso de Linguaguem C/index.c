b6fd2044b39ebbc54f1e84eadb5329525902e7fb
Cauê Alves <cauealvesb4@gmail.com>
2025-12-01 15:31:45 -0300

    Formatações

--- C/Curso de Linguaguem C/index.c
+++ C/Curso de Linguaguem C/index.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello World!\n");
-};
+    printf("Olá, tenho 19 anos e programo.\n");
+
+    printf("Valor do inteiro: %d. \n", 10);
+
+    printf("Valor flutuante: %f. \n", 12.43874763);
+
+    printf("Valor flutuante com duas casa deciamais: %.2f. \n", 12.43874763);
+
+    printf("Valor char: %c. \n", 'c');
+
+    printf("Valor string: %s. \n", "Cauê");
+
+    system("pause");
+}
